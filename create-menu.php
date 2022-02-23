f049fd1687d40aedc2bae65bc460c5943a2ac180
PoemaPolloshka <93519598+PoemaPolloshka@users.noreply.github.com>
2022-02-23 18:03:06 +0100

    Add files via upload

---
+++ create-menu.php
@@ -0,0 +1,20 @@
+<?php 
+    require_once '../Controllers/MenuController.php';
+    $produkti = new MenuController;
+    if(isset($_POST['submit'])){
+        $produkti -> insert ($_POST);
+    }
+?>
+
+<div>
+    <form method="POST">
+            Title:
+            <input type="text" name="content">
+            <br>
+            Image:
+            <input type="file" name="image">
+            <!--Content:
+            <textarea name ="body" cols="30" rows="10"></textarea>-->
+            <input type="submit" name="submit" value="Save">
+        </from>
+</div>
